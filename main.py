a025929ca22ea699faee1312ac27bef4ed220de5
shunlog <artiombalan331@gmail.com>
2023-11-09 19:51:51 +0200

    add iterator pattern

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
 #!/usr/bin/env python3
-from typing import List, Tuple, Any
-from observer import Observer, Subject
+from typing import List, Tuple, Any, Union
+from dataclasses import dataclass, field
 import logging
+
+from observer import Observer, Subject
+from icecream import ic
+
+class IterableElem:
+    '''Children of this class must have an iterable `items` attribute'''
+    def __iter__(self):
+        return next(self)
+
+    def __next__(self):
+        yield self
+        for item in self.items:
+            for i in item:
+                yield i
+
+class NonIterable:
+    def __iter__(self):
+        return next(self)
+
+    def __next__(self):
+        yield self
+
+
+@dataclass
+class Paragraph(NonIterable):
+    text: str
+
+@dataclass
+class ListItem(NonIterable):
+    text: str
+
+@dataclass
+class ListElem(IterableElem):
+    items: List[ListItem] = field(default_factory=list)
+
+@dataclass
+class Heading(IterableElem):
+    text: str
+    level: int
+    items: List[Union[Paragraph, 'Heading', ListElem]] = field(default_factory=list)
+
+@dataclass
+class DocumentContent(IterableElem):
+    items: List[Union[Paragraph, Heading, ListElem]] = field(default_factory=list)
+
 
 
 class GUI(Observer):
@@ -18,9 +63,13 @@ class Collaborator(Observer):
 
 
 class Document(Subject):
+    def __init__(self, content: DocumentContent = DocumentContent(),
+                 obs : List[Observer] = []):
+        super().__init__(obs)
+        self.content = content
+
     def edit(self) -> None:
         logging.info("Edited document")
-
         self.notify_observers()
 
 
@@ -28,7 +77,19 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format='%(levelname)s: %(message)s')
 
-    doc = Document()
+    doc = Document(DocumentContent([
+        Paragraph("This is the Introduction"),
+        Heading("First heading", 1, [
+            Paragraph("Inside heading"),
+            ListElem([
+                ListItem("Item 1"),
+                ListItem("Item 2")
+            ])
+        ])
+    ]))
+
+    ic(list(doc.content))
+
     o1 = GUI()
     o2 = Collaborator()
 
